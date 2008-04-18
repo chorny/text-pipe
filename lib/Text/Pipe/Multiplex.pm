0fec6776346418936549bcd0bfcecc48ec2ca325
Marcel Gruenauer <hanekomu@gmail.com>
2008-04-18 15:03:42 +0000

    r6191@nbgr:  marcel | 2008-04-18 17:01:51 +0200  lang/perl/Text-Pipe: initial commit
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Text-Pipe/tags/Text-Pipe-0.05@9815 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/Text/Pipe/Multiplex.pm
@@ -0,0 +1,69 @@
+package Text::Pipe::Multiplex;
+
+use warnings;
+use strict;
+
+
+our $VERSION = '0.05';
+
+
+use base 'Text::Pipe::Base';
+
+    
+__PACKAGE__
+    ->mk_array_accessors(qw(pipes));
+
+
+sub filter {
+    my ($self, $input) = @_;
+    [ map { $_->filter($input) } $self->pipes ];
+}
+
+
+sub deep_count {
+    my $self = shift;
+    my $count = 0;
+
+    for my $pipe ($self->pipes) {
+        if ($pipe->can('deep_count')) {
+            $count += $pipe->deep_count;
+        } else {
+            $count++;
+        }
+    }
+
+    $count;
+}
+
+
+1;
+
+
+__END__
+
+{% USE p = PodGenerated %}
+
+=head1 NAME
+
+{% p.package %} - Common text filter API
+
+=head1 SYNOPSIS
+
+    {% p.package %}->new;
+
+=head1 DESCRIPTION
+
+{% p.write_inheritance %}
+
+=head1 METHODS
+
+=over 4
+
+{% p.write_methods %}
+
+=back
+
+{% PROCESS standard_pod %}
+
+=cut
+
