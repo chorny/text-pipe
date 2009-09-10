d8d2bca474bf0905ca330ead9825cbe9aada83c7
Marcel Gruenauer <hanekomu@gmail.com>
2009-09-10 14:07:31 +0200

    repo maintenance

--- lib/Text/Pipe/List/Sum.pm
+++ lib/Text/Pipe/List/Sum.pm
@@ -133,7 +133,7 @@ Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007-2008 by the authors.
+Copyright 2007-2009 by the authors.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
